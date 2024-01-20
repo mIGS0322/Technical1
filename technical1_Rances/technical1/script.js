67514d38e81747ac0ba3f1ca5402366986bf9a93
Miguel Rances <139059801+mIGS0322@users.noreply.github.com>
2024-01-20 13:20:18 +0800

    Add files via upload

---
+++ technical1_Rances/technical1/script.js
@@ -0,0 +1,5 @@
+function changeColor() {
+    var resumeContainer = document.querySelector('.resume-container');
+    resumeContainer.style.backgroundColor = '#000'; 
+    resumeContainer.style.color = '#fff';
+}
